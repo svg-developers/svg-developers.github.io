f8ae599a57f339c31c3153ef1eaafbd22e924f4d
Peter Schonefeld <peter@vectorshapes.com>
2013-05-22 23:19:27 +1000

    incl. dart page template

---
+++ dart_projects/dart_page_template/web/dart_page_template.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+//following is default dart code
+
+void main() {
+  query("#sample_text_id")
+    ..text = "Click me!"
+    ..onClick.listen(reverseText);
+}
+
+void reverseText(MouseEvent event) {
+  var text = query("#sample_text_id").text;
+  var buffer = new StringBuffer();
+  for (int i = text.length - 1; i >= 0; i--) {
+    buffer.write(text[i]);
+  }
+  query("#sample_text_id").text = buffer.toString();
+}
